271429520463a0d3ee90d464534964119008ffcb
Tempo <Tempo@tempo-imac.local>
2016-10-24 11:31:07 +0800

    Initial Commit

---
+++ 防盗/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  防盗
+//
+//  Created by Tempo on 16/10/24.
+//  Copyright © 2016年 Tempo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 防盗/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  防盗
+//
+//  Created by Tempo on 16/10/24.
+//  Copyright © 2016年 Tempo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
